d67c8318d0cd5de656b4569eaf2e682ebf74dcf2
Francisco Sole <103468460+FranciscoSole@users.noreply.github.com>
2026-04-11 17:18:04 -0300

    Delete src/facultad/apps2 directory

--- src/facultad/apps2/src/configs/pagination.config.js
+++
@@ -1,8 +0,0 @@
-const { env } = require('./env.config');
-
-const paginationConfig = {
-    defaultPage: 1,
-    defaultPageSize: Number(env.paginationDefaultPageSize),
-}
-
-module.exports = { paginationConfig };
--- src/facultad/apps2/src/errors/not-found-error.js
+++
@@ -1,9 +0,0 @@
-const { AppError } = require('@/errors/app-error');
-
-class NotFoundError extends AppError {
-  constructor(message = 'Resource not found') {
-    super(message, 404);
-  }
-}
-
-module.exports = { NotFoundError };
